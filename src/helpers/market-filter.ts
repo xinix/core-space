31a9ef190c8454dd30b660b5834b7dfe9892ca89
Nico <nvdw007@gmail.com>
2024-09-16 17:14:17 +0200

    Release 2.0.2

--- src/helpers/market-filter.ts
+++ src/helpers/market-filter.ts
@@ -2,63 +2,66 @@ import { CoreSpaceToken, TradingPost } from '@/tokens/types'
 
 export function marketFilter(
     post: TradingPost,
-    items: CoreSpaceToken[]
+    items: CoreSpaceToken[],
 ): CoreSpaceToken[] {
     return filter[post](items)
+}
+
+function isWithin(
+    value: undefined | number | number[],
+    min: number,
+    max: number,
+): boolean {
+    if (value == undefined) return false
+    if (Array.isArray(value)) {
+        for (const num of value) {
+            if (num >= min && num <= max) {
+                return true
+            }
+        }
+        return false
+    }
+    return value >= min && value <= max
 }
 
 const filter = {
     'bs-mart': (items: CoreSpaceToken[]): CoreSpaceToken[] => {
         /* all items with buy <= 4UA */
         return items.filter(
-            (a) => a.buy != undefined && a.buy <= 4 && a.crystals == undefined
+            (a) => isWithin(a.buy, 4, 4) && a.crystals == undefined,
         )
     },
     'ucp': (items: CoreSpaceToken[]): CoreSpaceToken[] => {
         /* close assault with buy 5-9UA */
         return items.filter(
-            (a) =>
-                a.buy != undefined &&
-                a.buy >= 5 &&
-                a.buy <= 9 &&
-                a.close != undefined
+            (a) => isWithin(a.buy, 5, 9) && a.close != undefined,
         )
     },
     'claw-store': (items: CoreSpaceToken[]): CoreSpaceToken[] => {
         /* ranged assault with buy 5-9UA */
         return items.filter(
             (a) =>
-                a.buy != undefined &&
-                a.buy >= 5 &&
-                a.buy <= 9 &&
+                isWithin(a.buy, 5, 9) &&
                 (a.short != undefined ||
                     a.medium != undefined ||
-                    a.long != undefined)
+                    a.long != undefined),
         )
     },
     'armourote': (items: CoreSpaceToken[]): CoreSpaceToken[] => {
         /* armour with buy 5-9UA */
         return items.filter(
-            (a) =>
-                a.buy != undefined &&
-                a.buy >= 5 &&
-                a.buy <= 9 &&
-                a.color === 'yellow'
+            (a) => isWithin(a.buy, 5, 9) && a.color === 'yellow',
         )
     },
     'tech-noir': (items: CoreSpaceToken[]): CoreSpaceToken[] => {
         /* special items  with buy 5-9UA */
         return items.filter(
-            (a) =>
-                a.buy != undefined &&
-                a.buy >= 5 &&
-                a.buy <= 9 &&
-                a.color === 'orange'
+            (a) => isWithin(a.buy, 5, 9) && a.color === 'orange',
         )
     },
     'giger': (items: CoreSpaceToken[]): CoreSpaceToken[] => {
         /* all items with buy +10UA */
-        return items.filter((a) => a.buy != undefined && a.buy >= 10)
+        return items.filter((a) => isWithin(a.buy, 10, 10))
     },
     'workshop': (items: CoreSpaceToken[]): CoreSpaceToken[] => {
         /* purge items  */
