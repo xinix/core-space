e6ae59218ac0fff8e514f7e176b9a4bc3092e1bb
Nico <nvdw007@gmail.com>
2024-09-16 16:32:26 +0200

    FIX update breach pistol price from first born

--- build/tokens.cjs
+++ build/tokens.cjs
@@ -1,7 +1,7 @@
 const lib = require('./lib.cjs')
 
 const INPUT_URL = 'src/tokens/yaml/'
-const OUTPUT_URL = 'public/data/'
+const OUTPUT_URL = 'src/assets/data/'
 
 console.log('*** Welcome to the TOKENS yaml 2 json parser ***')
 lib.convertYaml2Json(INPUT_URL, OUTPUT_URL)
